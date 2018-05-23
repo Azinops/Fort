b20a7594c8e426a434807f42b53051e28652dbca
azinops <edgar@baucher.net>
2018-05-23 20:16:40 +0200

    retest

--- affichage.h
+++ affichage.h
@@ -7,7 +7,7 @@ void afficher_blocs(fenetre f,carre c[NBRE_CASES_Y][NBRE_CASES_X],ALLEGRO_BITMAP
 void afficher_selection(ALLEGRO_BITMAP* bitmap,point_case souris,fenetre f);
 void afficher_blocs_selec(ALLEGRO_BITMAP* selec,ALLEGRO_BITMAP* blocs[],joueur* j,double taille,fenetre f,item it[],ALLEGRO_MOUSE_STATE mouse,ALLEGRO_BITMAP* explications[]);
 void afficher_objet_anime(objet_anime* o);
-void interface_competences(CLAVIER,int* interface_du_jeu,int* fond,SOURIS,ALLEGRO_BITMAP* selection_jaune);
+void interface_competences(CLAVIER,int* interface_du_jeu,int* fond,SOURIS,ALLEGRO_BITMAP* selection_jaune,joueur j,ALLEGRO_FONT* police);
 void afficher_objet_fixe(objet_fixe o);
 void tracer_ligne_entre_objets(objet_fixe o1, objet_fixe o2,double epaisseur, ALLEGRO_COLOR couleur);
 void connexions(objet_fixe o[],int nbre_objets,joueur j);
@@ -17,7 +17,7 @@ void afficher_fusees(fusee_missile f[]);
 void afficher_objet_anime_en_masse(objet_anime o[],int nbre_objets);
 void afficher_objet_fixe_en_masse(objet_fixe o[],int nbre_objets);
 void selection_objet_jaune(ALLEGRO_BITMAP* selection_jaune,objet_fixe o,SOURIS);
-int afficher_inventaire_et_renvoyer_id_item_si_clic(ALLEGRO_BITMAP* inventaire,ALLEGRO_BITMAP* case_inv,int nbre_cases_x,int nbre_cases_y,double taille,double x,double y,ALLEGRO_BITMAP* icones[],joueur jo,ALLEGRO_BITMAP* selection,SOURIS,int item_selectione_initial,ALLEGRO_FONT* police,ALLEGRO_COLOR couleur,item_missile missiles[]);
+int afficher_inventaire_et_renvoyer_id_item_si_clic(ALLEGRO_BITMAP* inventaire,ALLEGRO_BITMAP* case_inv,int nbre_cases_x,int nbre_cases_y,double taille,double x,double y,ALLEGRO_BITMAP* icones[],joueur jo,ALLEGRO_BITMAP* selection,SOURIS,int item_selectione_initial,ALLEGRO_FONT* police,ALLEGRO_COLOR couleur,item_missile missiles[],ALLEGRO_BITMAP* explications[]);
 void afficher_scores(ALLEGRO_FONT* police,ALLEGRO_COLOR couleur,joueur j,double x);
 void afficher_barre_xp(ALLEGRO_BITMAP* barre[],joueur j,ALLEGRO_COLOR couleur,ALLEGRO_FONT* police);
 void afficher_pts_competences(joueur j,ALLEGRO_COLOR couleur,ALLEGRO_FONT* police);
